adc34e05ca0905ad1be3704f733367e255ec93fa
--Dill-- <44283246+Dallas-Oliver@users.noreply.github.com>
2019-01-09 00:38:31 -0500

    Update index.js

--- obs-api/index.js
+++ obs-api/index.js
@@ -1,7 +1,7 @@
 "use strict";
 exports.__esModule = true;
 // TODO: can we replace this with const?
-var obs = window['require']('obs-studio-node');
+const obs = window['require']('obs-studio-node');
 
 /* Use for...in operator to perfectly mirror the osn module */
 for (let entry in obs) {
